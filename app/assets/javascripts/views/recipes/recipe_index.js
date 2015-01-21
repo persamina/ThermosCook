83acd2869d8cf4ed03f217335c4f6d1f14ff6605
Andrew Petaisto <ahpetaisto@gmail.com>
2015-01-21 09:50:41 -0600

    reduced window resize trigger count

--- app/assets/javascripts/views/recipes/recipe_index.js
+++ app/assets/javascripts/views/recipes/recipe_index.js
@@ -83,41 +83,47 @@ ThermosCook.Views.RecipeIndex = Backbone.View.extend({
     console.log("like tile");
   },
   positionTiles: function() {
+
     var recipeIndex = this;
     var tileWidth = 250;
-    var totalHorizontalMargin = 20;
-    var totalVerticalMargin = 20;
-    var numberPlaced = 0;
-    var numberPerRow = Math.floor(recipeIndex.$("#tiles").width()/(tileWidth + totalHorizontalMargin));
-    var numberToPlace = recipeIndex.$("#tiles").children().length;
-    var lowestPosition = new Array(numberPerRow);
+    var previousWidth = this.$("div#data").data().previousWidth;
+    if (Math.abs(recipeIndex.$("#tiles").width() - previousWidth) > tileWidth) {
+      var totalHorizontalMargin = 20;
+      var totalVerticalMargin = 20;
+      var numberPlaced = 0;
+      var numberPerRow = Math.floor(recipeIndex.$("#tiles").width()/(tileWidth + totalHorizontalMargin));
+      var numberToPlace = recipeIndex.$("#tiles").children().length;
+      var lowestPosition = new Array(numberPerRow);
 
 
-    recipeIndex.$("#tiles").children().each(function(index, tile) {
-      var lPIndex = numberPlaced % (numberPerRow);
+      recipeIndex.$("#tiles").children().each(function(index, tile) {
+        var lPIndex = numberPlaced % (numberPerRow);
 
-      var positions = {
-        leftPosition: 0,
-        topPosition: 0
-      };
-      if (numberPlaced >= numberPerRow) {
-        positions.topPosition = lowestPosition[lPIndex];
-      } 
+        var positions = {
+          leftPosition: 0,
+          topPosition: 0
+        };
+        if (numberPlaced >= numberPerRow) {
+          positions.topPosition = lowestPosition[lPIndex];
+        } 
 
-      positions.leftPosition = (tileWidth + totalHorizontalMargin) * (numberPlaced % numberPerRow);
+        positions.leftPosition = (tileWidth + totalHorizontalMargin) * (numberPlaced % numberPerRow);
 
-      //$($(tile).children()).attr("style", "top: " + positions.topPosition + "px;" + 
-      //                                   "left: "+  positions.leftPosition + "px; ");
+        //$($(tile).children()).attr("style", "top: " + positions.topPosition + "px;" + 
+        //                                   "left: "+  positions.leftPosition + "px; ");
 
-      recipeIndex.$($(tile).children()).animate({top: positions.topPosition + "px",
-                                     left: positions.leftPosition + "px" }, "slow");
+        recipeIndex.$($(tile).children()).animate({top: positions.topPosition + "px",
+                                       left: positions.leftPosition + "px" }, "slow");
 
-      lowestPosition[lPIndex] = positions.topPosition + 
-                                recipeIndex.$($("div.tile")[numberPlaced]).height() +
-                                totalVerticalMargin;
-      numberPlaced++;
+        lowestPosition[lPIndex] = positions.topPosition + 
+                                  recipeIndex.$($("div.tile")[numberPlaced]).height() +
+                                  totalVerticalMargin;
+        numberPlaced++;
 
-    });
+      });
+
+      recipeIndex.$("div#data").data("previousWidth", recipeIndex.$("#tiles").width());
+    }
 
   },
 });
